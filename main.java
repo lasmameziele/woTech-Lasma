468600c4e4ddd50207f7088f5e762465efc1bc64
lasmameziele <165931777+lasmameziele@users.noreply.github.com>
2024-04-03 19:19:27 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,28 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+//winter, spring, summer, autumn
+    //warm jacket, t-shirt ,swimming suite, rain coat
+  
+    double temp = -15.0;
+
+  if (temperature <= 5) {
+      System.out.println("Wear a super warm coat.");
+  } else if (temperature <= 15) {
+      System.out.println("Wear a warm coat.");
+  } else if (temperature <= 30) {
+      System.out.println("Wear normal clothing.");
+  } else {
+      System.out.println("Wear a cooling jacket.");
+  }
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
